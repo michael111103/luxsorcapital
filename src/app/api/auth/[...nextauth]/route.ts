035081ef8d724a25f235fcd0f28254c57b6aa9d9
Michael Ivanov <56745304+michael111103@users.noreply.github.com>
2026-07-11 23:35:46 +0700

    Delete src/app/api directory

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-// src/app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth/next";
-import { nextAuthOptions } from "../../../../lib/nextAuthOptions";
-
-const handler = NextAuth(nextAuthOptions);
-export { handler as GET, handler as POST };
