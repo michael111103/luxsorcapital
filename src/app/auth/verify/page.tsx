b868313e46158aca593359bb2c3db6193475d8f3
Michael Ivanov <56745304+michael111103@users.noreply.github.com>
2026-07-11 23:35:58 +0700

    Delete src/app/auth directory

--- src/app/auth/verify/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-
-import React from "react";
-import VerifyClient from "./VerifyClient";
-
-export default function VerifyPage() {
-  return <VerifyClient />;
-}
